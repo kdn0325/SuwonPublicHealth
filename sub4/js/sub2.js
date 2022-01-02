f4ac2974ab7c4d882a398f53a308c56611f28bc3
kdn0325 <sexy-ehdsud@nate.com>
2022-01-02 19:15:23 +0900

    commit

---
+++ sub4/js/sub2.js
@@ -0,0 +1,26 @@
+var menu = $('#nav>.nav_gnb>li')
+menu.on({mouseover:function(){
+    menu.find('.gnb_sub').css({height:'380px'}).slideDown()
+},mouseleave:function(){
+    menu.find('.gnb_sub').css({height:'0px',display:'none'})
+}})
+menu.on({mouseover:function(){
+    $(this).find('.gnb_sub').css({backgroundColor:'rgb(217,230,246)'})
+},mouseleave:function(){
+    $(this).find('.gnb_sub').css({backgroundColor:'#f3f4f8'})
+}});
+
+var sidemenu = $('.smenu>ul>li')
+var sidesub = $('.smenu>ul>li>ul')
+
+sidemenu.click(function(){
+    var tg = $(this);
+    var i = tg.index();
+
+    sidemenu.removeClass('active');
+    tg.addClass('active');
+
+    sidesub.css({display:'none'})
+    sidesub.eq(i).css({display:'block'})
+
+})
