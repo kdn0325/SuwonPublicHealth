bcfc2746f5090ccc1c9d51dcc8795a8026be346b
kdn0325 <sexy-ehdsud@nate.com>
2022-05-18 20:10:25 +0900

    code-desc

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+
+/* 메뉴 슬라이드 오버 */
+
 var menu = $('#nav>.nav_gnb')
 menu.on({mouseover:function(){
     menu.find('.gnb_sub').css({height:'380px'}).slideDown()
 },mouseleave:function(){
     menu.find('.gnb_sub').css({height:'0px',display:'none'}).slideUp()
 }})
+
+/* 서브메뉴 오버시 컬러 변경 */
 
 var submenu =$('#nav>.nav_gnb>li')
 submenu.on({mouseover:function(){
@@ -12,32 +17,19 @@ submenu.on({mouseover:function(){
     $(this).find('.gnb_sub').css({backgroundColor:'#f3f4f8'})
 }})
 
-$('.bxslider1').bxSlider({
-    auto: true,
-    autoControls: true,
-    stopAutoOnClick: true,
-    pager: true,
-    slideWidth: 568
-  });
-$('.bxslider2').bxSlider({
-    auto: true,
-    autoControls: true,
-    stopAutoOnClick: true,
-    pager: true,
-    slideWidth: 381,
-    mode:'fade'
-  });
+/*  사이드바 클릭시 서브 메뉴 */
 
-var tabmenu = $('.tab ul li')
-var health_tab= $('.health_tab>div')
+var sidemenu = $('.smenu>ul>li')
+var sidesub = $('.smenu>ul>li>ul')
 
-tabmenu.click(function(){
-    var tg = $(this)
-    var i = tg.index()
+sidemenu.click(function(){
+    var tg = $(this);
+    var i = tg.index();
 
-    tabmenu.removeClass('active')
-    tg.addClass('active')
+    sidemenu.removeClass('active');
+    tg.addClass('active');
 
-    health_tab.css({display:'none'})
-    health_tab.eq(i).css({display:'block'})
+    sidesub.css({display:'none'})
+    sidesub.eq(i).css({display:'block'})
+
 })
